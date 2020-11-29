b6ea21d043768c9c460195138b3370b059da10cb
banakonda <ft1.tomasz@gmail.com>
2020-11-29 19:25:49 +0100

    Fix errors

--- src/app/subjects/create-subject/step3/create-subject-step-three.component.ts
+++ src/app/subjects/create-subject/step3/create-subject-step-three.component.ts
@@ -12,45 +12,45 @@ export class CreateSubjectStepThreeComponent {
   @Input() newSubject: Subject;
 
   get classL(): string {
-    if (this.newSubject.schedule.lectures.enabled) {
+    if (this.newSubject.schedule.lectures.enabled && this.newSubject.schedule.lectures.classRooms) {
       return this.newSubject.schedule.lectures.classRooms.join(', ');
     }
   }
   set classL(value: string) {
-    if (this.newSubject.schedule.lectures.enabled) {
+    if (this.newSubject.schedule.lectures.enabled && this.newSubject.schedule.lectures.classRooms) {
       this.newSubject.schedule.lectures.classRooms = value.replace(' ', '').split(',');
     }
   }
 
   get classE(): string {
-    if (this.newSubject.schedule.exercise.enabled) {
+    if (this.newSubject.schedule.exercise.enabled && this.newSubject.schedule.exercise.classRooms) {
       return this.newSubject.schedule.exercise.classRooms.join(', ');
     }
   }
   set classE(value: string) {
-    if (this.newSubject.schedule.exercise.enabled) {
+    if (this.newSubject.schedule.exercise.enabled && this.newSubject.schedule.exercise.classRooms) {
       this.newSubject.schedule.exercise.classRooms = value.replace(' ', '').split(',');
     }
   }
 
   get classLab(): string {
-    if (this.newSubject.schedule.laboratories.enabled) {
+    if (this.newSubject.schedule.laboratories.enabled && this.newSubject.schedule.laboratories.classRooms) {
       return this.newSubject.schedule.laboratories.classRooms.join(', ');
     }
   }
   set classLab(value: string) {
-    if (this.newSubject.schedule.laboratories.enabled) {
+    if (this.newSubject.schedule.laboratories.enabled && this.newSubject.schedule.laboratories.classRooms) {
       this.newSubject.schedule.laboratories.classRooms = value.replace(' ', '').split(',');
     }
   }
 
   get classS(): string {
-    if (this.newSubject.schedule.seminars.enabled) {
+    if (this.newSubject.schedule.seminars.enabled && this.newSubject.schedule.seminars.classRooms) {
       return this.newSubject.schedule.seminars.classRooms.join(', ');
     }
   }
   set classS(value: string) {
-    if (this.newSubject.schedule.seminars.enabled) {
+    if (this.newSubject.schedule.seminars.enabled && this.newSubject.schedule.seminars.classRooms) {
       this.newSubject.schedule.seminars.classRooms = value.replace(' ', '').split(',');
     }
   }
